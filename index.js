75cd87a1746e5f4d9220774588c4b0469fe94b43
Benoît Poreaux <benoit@poreaux.me>
2022-06-21 11:18:09 +0200

    token stored separetly

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-console.log("w");
+console.log("working");
+const {token} = require("/home/pi/discordbot/creditential.json");
 token = "NzY2MDUwMDkxODQ4MzAyNjYz.GUTNcq.Wy8DAKL0HcAyoiIxzoOdbueL6sZWaYG1i02ANU"
-
-
 const { Client, Intents } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
